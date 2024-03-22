e4575d981b937afd1e59d173ddd11e76cb3c26d1
JoseAngelCh <jose.angel2605@outlook.com>
2024-03-22 15:56:36 -0400

    final

--- script.js
+++ script.js
@@ -11,15 +11,23 @@ axios.get(`https://leonardoapi.onrender.com/songs`)
 
             div.innerHTML = `
             <div class="title__artist">
-            <div class="img_album">
-            <img class="album__list" src="${song.path.front}" alt="">
-            <p>${song.title}</p>
+                <div class="img_album">
+                    <img class="album__list" src="${song.path.front}" alt="">
+                    <p>${song.title}</p>
+                </div>
+                <div class=title__agrupation>
+                    <p class="text_album">${song.album}</p>
+                    <p>${song.duration}</p>
+                </div>
             </div>
-            <p class="text_album">${song.album}</p>
-            <p>${song.duration}</p>
-            </div>
-
             `
+
+            div.addEventListener(`click`, () => {
+                document.getElementById(`current_song_img`).setAttribute(`src`,song.path.front)
+                document.getElementById(`control_audio`).setAttribute(`src`,song.path.audio)
+                document.getElementById(`current_title`).innerHTML = song.title
+                document.getElementById(`current_author`).innerHTML = song.author
+            })
 
             trackList.appendChild(div)
     } )
